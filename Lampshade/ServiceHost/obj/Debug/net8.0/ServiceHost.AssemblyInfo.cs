eec57a368f563483c78dc271c48dec16df35b479
Hessamsd <hessamsadeghi73@gmail.com>
2024-09-21 16:24:15 +0330

    Slide Management Implemented

--- Lampshade/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
+++ Lampshade/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b64742a92ba218c33c4909a4724b044dec5a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4cb699edabb7a935f74ca6487047edee7460be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
